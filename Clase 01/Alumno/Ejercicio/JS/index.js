31db10ee8360e7d812eb150c75680f4aaea48817
Uri_Zen <sendykuriel@gmail.com>
2019-06-01 13:43:56 -0300

    primer upload

---
+++ Clase 01/Alumno/Ejercicio/JS/index.js
@@ -0,0 +1,19 @@
+console.log('todo anda bien wachin')
+
+var nombreUsuario = prompt('ingrese su nombre');
+console.log('Hola ' + nombreUsuario + '!');
+
+var numerito = parseInt(prompt('Ingrese un número para sumar'));
+const numBase = 8;
+var sumado = numerito + numBase;
+console.log(sumado);
+
+var numerito2 = parseInt(prompt('Ingrese un número para restar'));
+const numBase2 = 8;
+var restado = numBase2 - numerito2;
+console.log(restado);
+
+var numerito3 = parseInt(prompt('Ingrese un número para sumar con otro'));
+var numerito4 = parseInt(prompt('Ingrese un segundo número para sumar con otro'));
+var sumado2 = numerito3 + numerito4
+console.log(sumado2);
