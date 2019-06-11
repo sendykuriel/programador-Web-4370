dd80001823115373d8781f292c0db54efb51fb94
Brian Alonso <brian.alonso.a@gmail.com>
2019-06-10 22:49:42 -0300

    Improve promedio function

--- Clase 04/Profe/Ejercicio/js/index.js
+++ Clase 04/Profe/Ejercicio/js/index.js
@@ -1,23 +1,18 @@
 // Datos iniciales de prueba
-var examResults = [ 7, 5, 6, 4, 3, 2, 8 ]
+var examResults = [7, 5, 6, 4, 3, 2, 8]
 
 // Creo mi función que recibe como parámetro un Array de números
-function calcProm (examResults) {
+function calcProm(examResults) {
   // Creo una variable para almacenar la nota y otra para la acumulación de la suma de las notas
-  var value
   var prom = 0
 
   // Recorro el Array de notas y en cada iteración sumo la nota en el acumulador
   for (var i = 0; i < examResults.length; i++) {
-    value = examResults[i]
-    prom = prom + value
+    prom = prom + examResults[i]
   }
-
   // Divido el total de la suma de todas la notas por el largo del Array (Cantidad de notas)
-  prom = prom / examResults.length
-
   // Devuelvo el resultado del promedio
-  return prom
+  return prom / examResults.length
 }
 
 // Uso mi función y guardo el resultado en una variable
